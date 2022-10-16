ed068974a930e364f69113951c6415d719ed4f84
Daleniant <vladusinskm@gmail.com>
2022-10-15 18:48:17 -0700

    Update DueDate.py

--- DueDate.py
+++ DueDate.py
@@ -1,56 +1,40 @@
 import tkinter as tk
+from tkinter import messagebox
+from tkinter import Toplevel
 from datetime import date
 
-class DueDate:
+
+class DueDate(Toplevel):
     duedates = []
-    def __init__(self, duedates, main):
-        super().__init__(master = main)
-        self.duedates = []
+
+    def __init__(self, main):
+        super().__init__(master=main)
         self.create_window()
-    def duedate(self):
+
+    def get_duedate(self):
         return self.duedates
-    
-    def is_valid_date(self,year, month, day):
-        today = date.today()
-        if year == int(today.strftime("%y")):
-            if month == int(today.strftime("%m")):
-                if day <= int(today.strftime("%d")):
-                    return False
-            elif month < int(today.strftime("%y")):
-                return False
-        elif year < int(today.strftime("%y")):
-            return False
-        day_count_for_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        if year%4==0 and (year%100 != 0 or year%400==0):
-            day_count_for_month[2] = 29
-        return (1 <= month <= 12 and 1 <= day <= day_count_for_month[month])
-    
+
     def update_dates(self, entries):
         self.duedates = []
         for en in entries:
-            if (en.get() == ''):
-                tk.messagebox.showwarning("Warning","Please do not leave any fields blank")
+            if en.get() == '':
+                messagebox.showwarning("Warning", "1. Please do not leave any fields blank")
                 self.duedates = []
                 break
-        if (entries[0].get() == "Test") or (entries[0].get() == "test"):
+        if entries[0].get().lower() in "test":
             self.duedates.append(1)
-        elif (entries[0].get() == "Assignment") or (entries[0].get() == "assignment"):
+        elif entries[0].get().lower() in "assignment":
             self.duedates.append(0)
         else:
-            tk.messagebox.showwarning("Warning","Please enter valid values")
-            self.duedates = []
-            return self.duedates
-        a,b,c = entries[1].get().split('/')
-        if not self.is_valid_date(int(c),int(a),int(b)):
-            tk.messagebox.showwarning("Warning","Please enter valid values")
+            messagebox.showwarning("Warning", "2. Please enter valid values")
             self.duedates = []
             return self.duedates
 
-        else:
-            self.duedates.append(entries[1].get())
+
+        self.duedates.append(entries[1].get())
         self.duedates.append(entries[2].get())
-        if not (entries[3].get() =="1" or entries[3].get() == "2" or entries[3].get() =="3"):
-            tk.messagebox.showwarning("Warning","Please enter valid values")
+        if not (entries[3].get() == "1" or entries[3].get() == "2" or entries[3].get() == "3"):
+            messagebox.showwarning("Warning", "Please enter valid values")
             self.duedates = []
             return self.duedates
         else:
@@ -63,58 +47,55 @@ class DueDate:
         btncolor = '#145DA0'
         textcolor = '#145DA0'
         btntextcolor = '#B1D4E0'
-        textbgcolor= '#E7E7E7'
-        self['bg']= bgcolor
-        
-        labels = self.add_labels()
+        self['bg'] = bgcolor
+
+        self.add_labels()
         entries = self.add_entries()
-        lbltitle=tk.Label(self, text="Enter Your Assignment/Test",bg = bgcolor, fg= textcolor, font=("Times", 20))
+        lbltitle = tk.Label(self, text="Enter Your Assignment/Test", bg=bgcolor, fg=textcolor, font=("Times", 20))
         lbltitle.pack()
 
-        lbltimereminder=tk.Label(self, text="Use MM/DD/YY",bg = bgcolor, fg= textcolor, font=("Times", 15))
+        lbltimereminder = tk.Label(self, text="Use MM/DD/YY", bg=bgcolor, fg=textcolor, font=("Times", 15))
         lbltimereminder.place(x=500, y=100)
-        
-        lbldiffreminder=tk.Label(self,text="On a scale of 1 to 3",bg = bgcolor, fg= textcolor, font=("Times", 15))
+
+        lbldiffreminder = tk.Label(self, text="On a scale of 1 to 3", bg=bgcolor, fg=textcolor, font=("Times", 15))
         lbldiffreminder.place(x=500, y=200)
-        
-        addbtn = tk.Button(self,text="Add", fg=btntextcolor, bg = btncolor, font=("Times", 10), command=lambda: [self.update_dates(entries),self.destroy()])
+
+        addbtn = tk.Button(self, text="Add", fg=btntextcolor, bg=btncolor, font=("Times", 10),
+                           command=lambda: [self.update_dates(entries), self.destroy()])
         addbtn.place(x=330, y=250)
-        
-        
+
     def add_labels(self):
         bgcolor = '#B1D4E0'
         textcolor = '#145DA0'
-        lblaort=tk.Label(self,text="Assignment or Test?",bg = bgcolor, fg= textcolor, font=("Times", 15))
+        lblaort = tk.Label(self, text="Assignment or Test?", bg=bgcolor, fg=textcolor, font=("Times", 15))
         lblaort.place(x=20, y=50)
-        
-        lbltime=tk.Label(self,text="When is it due?",bg = bgcolor, fg= textcolor, font=("Times", 15))
+
+        lbltime = tk.Label(self, text="When is it due?", bg=bgcolor, fg=textcolor, font=("Times", 15))
         lbltime.place(x=20, y=100)
 
-        lblclass=tk.Label(self,text="What class is it for?",bg = bgcolor, fg= textcolor, font=("Times", 15))
+        lblclass = tk.Label(self, text="What class is it for?", bg=bgcolor, fg=textcolor, font=("Times", 15))
         lblclass.place(x=20, y=150)
 
-        lbldifficulty=tk.Label(self,text="How hard is it?",bg = bgcolor, fg= textcolor, font=("Times", 15))
+        lbldifficulty = tk.Label(self, text="How hard is it?", bg=bgcolor, fg=textcolor, font=("Times", 15))
         lbldifficulty.place(x=20, y=200)
-        
-        return [lblaort,lbltime,lblclass,lbldifficulty]
-    
+
     def add_entries(self):
-        textbgcolor= '#E7E7E7'
-        aortentry= tk.Entry(self,highlightthickness=2,bg = textbgcolor)
-        aortentry.place(x = 275, y= 55)
-        
-        timeentry= tk.Entry(self,highlightthickness=2,bg = textbgcolor)
-        timeentry.place(x = 275, y= 105)
+        textbgcolor = '#E7E7E7'
+        aortentry = tk.Entry(self, highlightthickness=2, bg=textbgcolor)
+        aortentry.place(x=275, y=55)
 
-        classentry= tk.Entry(self,highlightthickness=2,bg = textbgcolor)
-        classentry.place(x = 275, y= 155)
+        timeentry = tk.Entry(self, highlightthickness=2, bg=textbgcolor)
+        timeentry.place(x=275, y=105)
 
-        diffentry= tk.Entry(self,highlightthickness=2,bg = textbgcolor)
-        diffentry.place(x = 275, y= 205)
+        classentry = tk.Entry(self, highlightthickness=2, bg=textbgcolor)
+        classentry.place(x=275, y=155)
 
-        return [aortentry,timeentry,classentry,diffentry]
+        diffentry = tk.Entry(self, highlightthickness=2, bg=textbgcolor)
+        diffentry.place(x=275, y=205)
+
+        return [aortentry, timeentry, classentry, diffentry]
+
 
 if __name__ == '__main__':
-  addduedates = DueDate([])
-  print(addduedates.duedate())
-    
+    addduedates = DueDate([])
+    print(addduedates.duedate())
